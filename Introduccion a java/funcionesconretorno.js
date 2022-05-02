b0b43eaaf4b647bea8233333ed58fc4ce1aaa9ba
Federex436 <federicoparrotta11@gmail.com>
2022-05-02 20:54:21 -0300

    cambios

--- Introduccion a java/funcionesconretorno.js
+++ Introduccion a java/funcionesconretorno.js
@@ -1,1 +1,11 @@
-    
+function sumar(a,b) {
+ const resultado = parseInt(a) + parseInt(b)
+ return resultado
+}
+
+function recolectaDatos() {
+    const dato1= document .getElementById("inp_d1").value
+    const dato2= document .getElementById("inp_d2").value
+//invoca a la funcion
+    const respuesta = sumar(dato1,dato2)
+}
