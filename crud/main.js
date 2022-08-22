d13a951702a55810900cf0ef7b08f87c3f346d60
Federex436 <federicoparrotta11@gmail.com>
2022-08-22 17:49:58 -0300

    a

--- crud/main.js
+++ crud/main.js
@@ -16,3 +16,37 @@ function guardar(){
         localStorage.setItem("lista_clientes", JSON.stringify(grilla_clientes))
     }
 }
+
+
+
+function editar(index){
+
+    let lista_clientes = JSON.parse(localStorage.getItem("lista_clientes"))
+
+    document.getElementById("inp_nombre").value = lista_clientes[index].nombre
+    document.getElementById("inp_apellido").value = lista_clientes[index].apellido
+    document.getElementById("inp_dni").value = lista_clientes[index].dni
+    
+    localStorage.setItem("indice",index)
+
+    document.getElementById("btn_guardar").style.display = 'none'
+    document.getElementById("btn_actualizar").style.display = 'block'
+
+}
+function actualizar_cliente(){
+    let lista_clientes = JSON.parse(localStorage.getItem("lista_clientes"))
+    let indice = localStorage.getItem("indice")
+    lista_clientes[indice].nombre = document.getElementById("inp_nombre").value
+    lista_clientes[indice].apellido = document.getElementById("inp_apellido").value
+    lista_clientes[indice].dni = document.getElementById("inp_dni").value
+    localStorage.setItem("lista_clientes",JSON.stringify(lista_clientes))
+    obtener_clientes()
+
+    document.getElementById("btn_actualizar").style.display = 'none'
+    document.getElementById("btn_guardar").style.display = 'block'
+    varciar_formulario()
+}
+
+function varciar_formulario(){
+    document.getElementById("form_client").reset()
+}
